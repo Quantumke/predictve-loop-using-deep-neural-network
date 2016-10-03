c32bc535ee790219884886c39c9a6dc435caf922
quantumke <nguruben@gmail.com>
2016-10-03 08:22:26 +0300

    problem statement

---
+++ layer.py
@@ -0,0 +1,18 @@
+"""
+In the matrix below try to predict the output column:
+INPUTS       OUTPUTS
+0 0 1          0
+1 1 1          1
+1 0 1          1
+0 1 1          0
+ use BackPropagation"""
+import numpy as np
+class predictloop():
+    def __init__(self, *args, **kwargs):
+        self.author="Benson Nguru"
+
+
+
+
+
+app = predictloop()
