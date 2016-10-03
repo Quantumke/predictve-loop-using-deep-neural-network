3765f9b53bb6aa0ee2479509b09032edc20e3e9f
quantumke <nguruben@gmail.com>
2016-10-03 08:27:25 +0300

    determined x and y input

--- layer.py
+++ layer.py
@@ -10,6 +10,19 @@ import numpy as np
 class predictloop():
     def __init__(self, *args, **kwargs):
         self.author="Benson Nguru"
+        self.nonlin()
+    #sigmoid function
+    def nonlin(x, deriv=False):
+        if(deriv=True):
+            return x *(1-x)
+        return 1/(1+np.exp(-x))
+    #input dataset
+    X=np.array([ [0,0,1],
+               [0,1,1],
+               [1,0,1],
+               [1,1,1] ])
+    #output  dataset
+    y=np.array([[0,0,1,1]]).T
 
 
 
